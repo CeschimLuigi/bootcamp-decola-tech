4a1fd76f5f1edd8d89d8a3fe129466441c60b15a
CeschimLuigi <luigifaggeceschim@gmail.com>
2025-02-18 15:01:32 -0300

    Initial commit

---
+++ src/main/java/com/LuigiCeschim/bootcamp_decola_tech/BootcampDecolaTechApplication.java
@@ -0,0 +1,13 @@
+package com.LuigiCeschim.bootcamp_decola_tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampDecolaTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampDecolaTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LuigiCeschim/bootcamp_decola_tech/BootcampDecolaTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LuigiCeschim.bootcamp_decola_tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampDecolaTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
